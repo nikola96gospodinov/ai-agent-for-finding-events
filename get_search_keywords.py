3a58d09028934dc5f5267af3247ddf222962c550
Nik Gospodinov <nikola96gospodinov@gmail.com>
2025-05-16 16:59:44 +0100

    feat: further refinements to the search query generator

--- get_search_keywords.py
+++ get_search_keywords.py
@@ -23,14 +23,16 @@ def get_search_keywords(user_profile: UserProfile, model: BaseChatModel) -> List
         The user's goals are: {goals}
         The user's age is: {age}. For example, if the user is 30, the age should be "30s". If the user is 25, the age should be "20s" etc.
 
-        Start with the user's goals and provide a search query for each goal. 
-        Certain goals might benefit from including the user's age in the search queries. For example, "30s make new friends" and "40s dating" are good search queries. At the same time, "20s business networking" or "tech 40s" are not good search queries as those goals are not relevant to the user's age.
+        Start with the user's goals and provide a search query for each goal.
+        Certain goals might benefit from including the user's age in the search queries. For example, "30s make new friends" and "40s dating" are good search queries. At the same time, "20s business networking" or "tech 40s" are not good search queries.
         
-        Then move on to the user's interests.
-        Some interests might be related to each other. For example, "tech" and "business" are related. "outdoors" and "hiking" are related. Make sure to combine interests that are related but don't force it.
+        Then move on to the user's interests. Don't include the user's age in the search queries for the interests.
+        Some interests might be related to each other. For example, "tech" and "business" are related. "outdoors" and "hiking" are related. Make sure to combine interests that are related but don't force it. For example combining unrelated interests like "tech outdoors" and "motorsports hiking" don't make good search queries.
 
         Limit the search queries to 4 words or less. The number of search queries should be between 5 and 20.
-        Don't include words like "events", "meetups" etc. in the search queries. Only include words like "networking" if they are extremely relevant to the rest of the search query but do not use excessively and don't use on its own.
+        Don't include words like "events", "meetups" etc. in the search queries.
+        Only include words like "networking" if they are extremely relevant to the rest of the search query but do not use excessively. 
+        Don't use "networking" on its own or with the user's age.
         Avoid having overly similar search queries that will potentially return the same events.
         
         The response should be a list of search queries separated by commas. For example: "tech, business, networking"
